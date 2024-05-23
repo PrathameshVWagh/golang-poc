69b23de86a9620d4fd94c80773dc61c366794600
prathamesh.wagh <prathamesh.wagh@datamato.com>
2024-05-23 17:25:12 +0530

    feat: add print demo implementation

---
+++ variable/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var name string = "Prathamesh"
+	fmt.Println("Name : ", name)
+
+	var mileage int64 = 3434
+	fmt.Println(mileage)
+
+	var condition bool = true
+	fmt.Println(condition)
+
+	var cost float64 = 334.34343
+	fmt.Println(cost)
+
+	const pi float64 = 3.14
+	fmt.Println(pi)
+
+	var privateVariable string = "hello"
+	fmt.Println(privateVariable)
+
+	var PublicVariable string = "Hi"
+	fmt.Println(PublicVariable)
+
+}
